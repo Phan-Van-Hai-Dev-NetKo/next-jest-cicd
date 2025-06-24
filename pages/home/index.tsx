aa7caa2152eef6bdcdc7787932a2a2c9f7828c0a
Phan-Van-Hai-Dev-NetKo <hai.phan@netko-solution.com>
2025-06-24 11:04:34 +0700

    test cicd 2

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App Update</title>
+        <title>Create Next App Update abc</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
